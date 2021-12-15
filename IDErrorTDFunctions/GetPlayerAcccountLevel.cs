9b8e0a5f7d7ba6ce89d29e0beb403791df75e5e0
Howllo <allenr64y@ymail.com>
2021-12-15 14:50:12 -0600

    Return player account level.

--- IDErrorTDFunctions/GetPlayerAcccountLevel.cs
+++ IDErrorTDFunctions/GetPlayerAcccountLevel.cs
@@ -1,13 +1,9 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using System.Net.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using PlayFab.Plugins.CloudScript;
 
 namespace IDErrorTDFunctions.Function
