03bf59e4d3f02688b82a1af26c2127dee1255a51
Howllo <allenr64y@ymail.com>
2022-02-20 23:17:46 -0600

    Gacha system that allows either 1 or 10 rolls. Automatically gets banner and information.

--- IDErrorTDFunctions/GachaFunction.cs
+++ IDErrorTDFunctions/GachaFunction.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -19,7 +20,7 @@ namespace IDErrorTDFunctions
     {
         static string DevSecretKey = Environment.GetEnvironmentVariable("PLAYFAB_DEV_SECRET_KEY", EnvironmentVariableTarget.Process);
         static string GetTitleInfo = Environment.GetEnvironmentVariable("PLAYFAB_TITLE_ID", EnvironmentVariableTarget.Process);
-        static int totalAmount = 1, Amount = 0;
+        static int totalAmount = 0, Amount = 0;
         static List<string> items = new List<string>();
 
         [FunctionName("GachaFunction")]
@@ -31,8 +32,11 @@ namespace IDErrorTDFunctions
             var args = context.FunctionArgument;
             var stringInfo = JsonConvert.SerializeObject(args);
             dynamic data = JObject.Parse(stringInfo);
-            string WhatBanner = data.WhatBanner;
-            string PlayerID = data.PlayerID;
+            string WhatBanner = data.WhatBanner;    
+            string PlayerID = data.PlayerID;        
+            string json = "";                            
+            int recruitmentFolderTotal = 0;
+            ItemInstance recruitmentFolderInstances = null;
             Amount = data.Amount;
 
             //Server Info
@@ -44,8 +48,28 @@ namespace IDErrorTDFunctions
             PlayFabAuthenticationContext titleContext = new PlayFabAuthenticationContext();
             var serverAPI = new PlayFabServerInstanceAPI(apiSetting, titleContext);
 
-            if(WhatBanner != null)
+            //User Inventory / User Information
+            var UserInformationRequest = new GetUserInventoryRequest()
             {
+                PlayFabId = PlayerID
+            };
+            var UserInformationResults = await serverAPI.GetUserInventoryAsync(UserInformationRequest);
+
+            //Get Total Amount of required materials.
+            foreach(var item in UserInformationResults.Result.Inventory)
+            {
+                if(item.ItemId == "recruitmentFolder")
+                {
+                    recruitmentFolderInstances = item;
+                    recruitmentFolderTotal = (int)item.RemainingUses;
+                    break;
+                }
+            }
+
+            //Start the gacha roll.
+            if (WhatBanner != null && recruitmentFolderTotal >= Amount)
+            {
+                //Roll for the total Amount
                 for (int i = 0; i < Amount; i++)
                 {
                     var request = new EvaluateRandomResultTableRequest()
@@ -59,19 +83,29 @@ namespace IDErrorTDFunctions
                     GrantItemToPlayer(results, PlayerID, log);
                     totalAmount++;
                 }
-            }
 
-            //Create a Json to be returned.
-            GetJsonInfo jsonCreator = new GetJsonInfo();
-            foreach(var arrayInfo in items)
-            {
-                jsonCreator.currentItems.Add(arrayInfo);
-            }
-            string json = JsonConvert.SerializeObject(jsonCreator);
+                //Removing Items from player inventory.
+                var RemoveUsesFromInventory = new ModifyItemUsesRequest()
+                {
+                    PlayFabId = PlayerID,
+                    ItemInstanceId = recruitmentFolderInstances.ItemInstanceId,
+                    UsesToAdd = -Amount * 5
+                };
+                var RemvoeUsesFromInventoryResults = await serverAPI.ModifyItemUsesAsync(RemoveUsesFromInventory);
+                log.LogInformation($"Removed {Amount * 5} of {recruitmentFolderInstances.ItemInstanceId} from player {PlayerID}");
 
+                //Create a Json to be returned.
+                GetJsonInfo jsonCreator = new GetJsonInfo();
+                foreach (var arrayInfo in items)
+                {
+                    jsonCreator.currentItems.Add(arrayInfo);
+                }
+                json = JsonConvert.SerializeObject(jsonCreator);
+            } 
             return new OkObjectResult(json);
         }
 
+        //Get total amount before requesting to REST API. This will reduce amount of calls to the API.
         public static void GrantItemToPlayer(PlayFabResult<EvaluateRandomResultTableResult> tableResult, string playFabID, ILogger log)
         {
             if (totalAmount <= Amount)
@@ -98,6 +132,11 @@ namespace IDErrorTDFunctions
 
     public class GetJsonInfo
     {
-        public List<string> currentItems; 
+        public List<string> currentItems = new List<string>(); 
+    }
+
+    public class GetJsonResult
+    {
+        public string NotEnough;
     }
 }
